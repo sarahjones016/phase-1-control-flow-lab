8d48e42120500441fb43ea6d9dbc29a01e680d09
Sarah Jones <sarahrjones8@gmail.com>
2023-03-19 00:59:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+    if (someValue < 400) {
+      return "This one is on me!";
+    } else if (someValue < 2000) {
+      return "That will be twenty bucks.";
+    } else if (someValue < 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else if (someValue > 2500) {
+      return "No can do.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  const message = destination === "NYC" ? "Ok, sounds good." : "No go.";
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  switch (generosity) {
+    case 'generous': 
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    case 'thanks for everything':
+      return "Bye."
+      break;
+  }
 }
