ed5141f93327d71ba5aa0e5b0940803dce3c6e47
Sarah Jones <sarahrjones8@gmail.com>
2023-03-19 15:14:15 -0400

    updated lab

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function scuberGreetingForFeet(someValue){
     if (someValue < 400) {
       return "This one is on me!";
-    } else if (someValue < 2000) {
+    } else if ((someValue > 400) && (someValue < 2000)) {
       return "That will be twenty bucks.";
-    } else if (someValue < 2500) {
+    } else if ((someValue > 2000) && (someValue < 2500)) {
       return "I will gladly take your thirty bucks.";
     } else if (someValue > 2500) {
       return "No can do.";
